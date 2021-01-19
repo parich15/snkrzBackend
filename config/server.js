5870a48f8ad1f4f56301c88d8b6d6b1dcbbfad48
parich15 <oscarparicio8@gmail.com>
2021-01-19 08:27:45 +0100

    Backend Snkrz

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f2418ae7a833a76cdc420d3914c5b545'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cbc6bdb2-5847-4ad8-9302-b98cdb3e2ae8'
+};
